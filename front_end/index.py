885783e1a163c1ace267e79bc979a907997717d5
Sebastian Echeverria <secheverriamartinez@csumb.edu>
2025-04-07 22:28:04 -0700

    Import Files
    User presses a button where they can choose which file they would like to import
    
    Read the imported file contents and add it into a .txt file

--- front_end/index.py
+++ front_end/index.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit)
+from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QFileDialog)
 
 application = QApplication([])
 
@@ -29,11 +29,31 @@ class NL2SQ(QWidget):
         self.submitLabel = QLabel("", self)
         layout.addWidget(self.submitLabel)
 
+        self.selectFileButton = QPushButton("Select a File", self)
+        self.selectFileButton.clicked.connect(self.get_file)
+        layout.addWidget(self.selectFileButton)
+
     def button_clicked(self):
         self.submitLabel.setText("TEXT HAS BEEN SUBMITTED")
         userInputedString = self.textbox.text()
         with open("userInput.txt", "w") as file:
             file.write(f"{userInputedString}")
+
+    def get_file(self):  
+        user_selected_file = QFileDialog.getOpenFileName(self, "Select a File", "", "Text Based Files (*.txt *.json);; Excel Files (*.xlsx *xls)")
+
+        actualFile = user_selected_file[0]
+
+        if actualFile:
+            # self.selectFileButton.setText(user_selected_file[0])
+            with open("user_selected_file_contents.txt", "w") as standardFile:
+                contents = ""
+                with open(f"{user_selected_file[0]}", "r") as selectedFile:
+                    contents = selectedFile.read()
+                standardFile.write(contents)
+        print(f"File contents of {actualFile} have been read and move to user_selected_file_contents.txt")
+                
+                
 
 
 my_win = NL2SQ()
