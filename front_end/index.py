852c6ff4416d6a3f4d5f4072d350cf35a500176f
Sebastian Echeverria <secheverriamartinez@csumb.edu>
2025-04-11 13:35:02 -0700

    User can add files

--- front_end/index.py
+++ front_end/index.py
@@ -1,4 +1,5 @@
 import sys
+import pandas
 from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QFileDialog)
 
 application = QApplication([])
@@ -29,7 +30,7 @@ class NL2SQ(QWidget):
         self.submitLabel = QLabel("", self)
         layout.addWidget(self.submitLabel)
 
-        self.selectFileButton = QPushButton("Select a File", self)
+        self.selectFileButton = QPushButton("Select a Schema", self)
         self.selectFileButton.clicked.connect(self.get_file)
         layout.addWidget(self.selectFileButton)
 
@@ -40,17 +41,27 @@ class NL2SQ(QWidget):
             file.write(f"{userInputedString}")
 
     def get_file(self):  
-        user_selected_file = QFileDialog.getOpenFileName(self, "Select a File", "", "Text Based Files (*.txt *.json);; Excel Files (*.xlsx *xls)")
+        # Include Pandas to convert .xlsx files into .txt files
+        # Check if that would be a good use of time or necessary 
+        user_selected_file = QFileDialog.getOpenFileName(self, "Select a File", "", "Text Based Files (*.txt *.json *.csv)")
 
         actualFile = user_selected_file[0]
 
         if actualFile:
             # self.selectFileButton.setText(user_selected_file[0])
+            # try:
             with open("user_selected_file_contents.txt", "w") as standardFile:
                 contents = ""
                 with open(f"{user_selected_file[0]}", "r") as selectedFile:
                     contents = selectedFile.read()
                 standardFile.write(contents)
+            # except:
+            #     excelfile = pandas.read_excel(actualFile, sheet_name=None)
+            #     # print(f"{excelfile.items()}")
+            #     with open("user_selected_file_contents.txt", "w") as standardFile:
+            #         for sheet, data in excelfile.items():
+            #             standardFile.write(f"Sheet: {sheet}\n\n {data.to_csv()}")
+
         print(f"File contents of {actualFile} have been read and move to user_selected_file_contents.txt")
                 
                 
